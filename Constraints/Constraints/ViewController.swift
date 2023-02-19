da006b011792b57ab8819d9f047c12f4f1acf448
MaximDmytruk <dmitruk.max@gmail.com>
2023-02-19 22:52:03 +0200

    first Commit

---
+++ Constraints/Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Maxim Dmytruk on 11.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
